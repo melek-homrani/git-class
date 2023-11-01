0c15365caba6e75e6d718637264e844bb06f61e5
melek.homrani <melek.homrani@esprit.tn>
2023-11-01 23:17:28 +0100

    update package
    update the folder structure,
    update main java file and
    update main java test file

---
+++ src/main/java/tn/esprit/store/StoreApplication.java
@@ -0,0 +1,9 @@
+package tn.esprit.store;
+
+import org.springframework.boot.SpringApplication;
+
+public class StoreApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StoreApplication.class, args);
+    }
+}
--- src/main/java/tn/esprit/test/test.java
+++
@@ -1,4 +0,0 @@
-package tn.esprit.test;
-
-public class test {
-}
